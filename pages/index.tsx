337618c37d9accb5721dbdd27d3a9399076e8258
Yuto Watanabe <yuto.w51942@gmail.com>
2021-07-17 20:11:49 +0900

    added eslint rules

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,11 @@
-/**
+/**********************************************************
  * Root page.
  *
  * @author Yuto Watanabe <yuto.w51942@gmail.com>
+ * @version 1.0.0
  *
  * Copyright (C) 2021 hello-slide
- */
+ **********************************************************/
 
 const Index = () => {
   return <div>Hello World</div>;
