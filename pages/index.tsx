c01507dc93b621bec06cf49d7ee8b7dc631b5594
Yuto Watanabe <yuto.w51942@gmail.com>
2021-07-17 19:05:25 +0900

    create projects

---
+++ pages/index.tsx
@@ -0,0 +1,13 @@
+/**
+ * Root page.
+ *
+ * @author Yuto Watanabe <yuto.w51942@gmail.com>
+ *
+ * Copyright (C) 2021 hello-slide
+ */
+
+const Index = () => {
+  return <div>Hello World</div>;
+};
+
+export default Index;
