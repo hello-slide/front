ebcef3d51672ea664e490186809a9e472527cbe5
Aqua Minato <sweet4182691@gmail.com>
2021-07-20 17:37:56 +0900

    fix: gtag.ts: pageview function: not arrow function add: gtag.ts: pageview function: JSDoc

--- utils/ga/gtag.ts
+++ utils/ga/gtag.ts
@@ -8,9 +8,13 @@
  **********************************************************/
 export const GA_TRACKING_ID = process.env.GOOGLE_ANALYTICS_ID || '';
 
-export const pageview = (url: string): void => {
-  if (!GA_TRACKING_ID) return;
-  window.gtag('config', GA_TRACKING_ID, {
-    page_path: url,
-  });
-};
+/**
+ * @param {string} url Path to the page. When overriding, the value must be prefixed with a slash "/".
+ */
+export function pageview(url: string) {
+  if (GA_TRACKING_ID) {
+    window.gtag('config', GA_TRACKING_ID, {
+      page_path: url,
+    });
+  }
+}
