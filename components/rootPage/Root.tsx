da888d383807fc871f0e562fcd84263bc9a5e356
Aqua Minato <sweet4182691@gmail.com>
2021-07-21 22:05:21 +0900

    add:Root.tsx: Logo

--- components/rootPage/Root.tsx
+++ components/rootPage/Root.tsx
@@ -6,13 +6,25 @@
  *
  * Copyright (C) 2021 hello-slide
  **********************************************************/
+import {Box} from '@chakra-ui/react';
 import React from 'react';
+import Logo from '../../assets/svgs/logo.svg';
 import Footer from '../common/Footer';
 
 const Root = () => {
   return (
     <React.Fragment>
-      Hello Slide
+      <Box
+        backgroundColor={'#333'}
+        width="100%"
+        mb={18}
+        p={2}
+        textAlign={'center'}
+      >
+        <Box>
+          <Logo width="30rem" />
+        </Box>
+      </Box>
       <Footer />
     </React.Fragment>
   );
