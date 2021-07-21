dbc9d1066e713e8826178185b2d290475a0e559d
Yuto Watanabe <yuto.w51942@gmail.com>
2021-07-22 01:35:19 +0900

    added dashboard page

---
+++ components/dashboard/DashboardPage.tsx
@@ -0,0 +1,15 @@
+/**********************************************************
+ * Dashboard page.
+ *
+ * @author Yuto Watanabe <yuto.w51942@gmail.com>
+ * @version 1.0.0
+ *
+ * Copyright (C) 2021 hello-slide
+ **********************************************************/
+import Page from '../common/Page';
+
+const DashboardPage = () => {
+  return <Page>Dashboard</Page>;
+};
+
+export default DashboardPage;
---
+++ pages/dashboard.tsx
@@ -0,0 +1,15 @@
+/**********************************************************
+ * Dashboard
+ *
+ * @author Yuto Watanabe <yuto.w51942@gmail.com>
+ * @version 1.0.0
+ *
+ * Copyright (C) 2021 hello-slide
+ **********************************************************/
+import DashboardPage from '../components/dashboard/DashboardPage';
+
+const Dashboard = () => {
+  return <DashboardPage />;
+};
+
+export default Dashboard;
