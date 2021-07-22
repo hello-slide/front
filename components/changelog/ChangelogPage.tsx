ec62fdf2c92f4bf073b78e5322489ec41a1bebbf
Yuto Watanabe <yuto.w51942@gmail.com>
2021-07-22 16:38:33 +0900

    added changelog page

---
+++ components/changelog/ChangelogPage.tsx
@@ -0,0 +1,14 @@
+/**********************************************************
+ * Changelog page.
+ *
+ * @author Yuto Watanabe <yuto.w51942@gmail.com>
+ * @version 1.0.0
+ *
+ * Copyright (C) 2021 hello-slide
+ **********************************************************/
+
+const ChangelogPage = () => {
+  return <>changelog</>;
+};
+
+export default ChangelogPage;
---
+++ pages/changelog.tsx
@@ -0,0 +1,15 @@
+/**********************************************************
+ * Change log
+ *
+ * @author Yuto Watanabe <yuto.w51942@gmail.com>
+ * @version 1.0.0
+ *
+ * Copyright (C) 2021 hello-slide
+ **********************************************************/
+import ChangelogPage from '../components/changelog/ChangelogPage';
+
+const Changelog = () => {
+  return <ChangelogPage />;
+};
+
+export default Changelog;
