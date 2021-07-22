fa02498b15936e01e014aa721152a6b8800c145b
Yuto Watanabe <yuto.w51942@gmail.com>
2021-07-22 16:39:54 +0900

    added usage page

---
+++ components/usage/UsagePage.tsx
@@ -0,0 +1,14 @@
+/**********************************************************
+ * Usage Page
+ *
+ * @author Yuto Watanabe <yuto.w51942@gmail.com>
+ * @version 1.0.0
+ *
+ * Copyright (C) 2021 hello-slide
+ **********************************************************/
+
+const UsagePage = () => {
+  return <>usage</>;
+};
+
+export default UsagePage;
---
+++ pages/usage.tsx
@@ -0,0 +1,15 @@
+/**********************************************************
+ * Usage
+ *
+ * @author Yuto Watanabe <yuto.w51942@gmail.com>
+ * @version 1.0.0
+ *
+ * Copyright (C) 2021 hello-slide
+ **********************************************************/
+import UsagePage from '../components/usage/UsagePage';
+
+const Usage = () => {
+  return <UsagePage />;
+};
+
+export default Usage;
