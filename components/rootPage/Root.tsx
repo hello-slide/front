393e11731e438a9fed19543ff57e3fb229d63ae9
Aqua Minato <sweet4182691@gmail.com>
2021-07-22 17:54:54 +0900

    add:Root.tsx:Lateral centering

--- components/rootPage/Root.tsx
+++ components/rootPage/Root.tsx
@@ -6,7 +6,7 @@
  *
  * Copyright (C) 2021 hello-slide
  **********************************************************/
-import {Box} from '@chakra-ui/react';
+import {Box, Flex} from '@chakra-ui/react';
 import React from 'react';
 import Logo from '../../assets/svgs/logo.svg';
 import Footer from '../common/Footer';
@@ -14,16 +14,10 @@ import Footer from '../common/Footer';
 const Root = () => {
   return (
     <React.Fragment>
-      <Box
-        backgroundColor={'#333'}
-        width="100%"
-        mb={18}
-        p={2}
-        textAlign={'center'}
-      >
-        <Box>
-          <Logo width="30rem" />
-        </Box>
+      <Box backgroundColor={'#333'} width="100vw" height="100vh" m={0} p={0}>
+        <Flex justifyContent="center">
+          <Logo width="30rem" my="50vh" />
+        </Flex>
       </Box>
       <Footer />
     </React.Fragment>
