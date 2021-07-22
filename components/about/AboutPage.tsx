d824a2f1afd39e5bac0238f0d34e5df7c53b170b
Yuto Watanabe <yuto.w51942@gmail.com>
2021-07-22 16:38:10 +0900

    added about page

---
+++ components/about/AboutPage.tsx
@@ -0,0 +1,14 @@
+/**********************************************************
+ * About page
+ *
+ * @author Yuto Watanabe <yuto.w51942@gmail.com>
+ * @version 1.0.0
+ *
+ * Copyright (C) 2021 hello-slide
+ **********************************************************/
+
+const AboutPage = () => {
+  return <>about</>;
+};
+
+export default AboutPage;
---
+++ pages/about.tsx
@@ -0,0 +1,15 @@
+/**********************************************************
+ * About
+ *
+ * @author Yuto Watanabe <yuto.w51942@gmail.com>
+ * @version 1.0.0
+ *
+ * Copyright (C) 2021 hello-slide
+ **********************************************************/
+import AboutPage from '../components/about/AboutPage';
+
+const About = () => {
+  return <AboutPage />;
+};
+
+export default About;
