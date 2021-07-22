ee0f0ab7a9cb24f150275ea3598f0f9158280b81
Yuto Watanabe <yuto.w51942@gmail.com>
2021-07-22 16:39:28 +0900

    added question page

---
+++ components/question/QuestionPage.tsx
@@ -0,0 +1,14 @@
+/**********************************************************
+ * Question page
+ *
+ * @author Yuto Watanabe <yuto.w51942@gmail.com>
+ * @version 1.0.0
+ *
+ * Copyright (C) 2021 hello-slide
+ **********************************************************/
+
+const QuestionPage = () => {
+  return <>question</>;
+};
+
+export default QuestionPage;
---
+++ pages/question.tsx
@@ -0,0 +1,15 @@
+/**********************************************************
+ * Question
+ *
+ * @author Yuto Watanabe <yuto.w51942@gmail.com>
+ * @version 1.0.0
+ *
+ * Copyright (C) 2021 hello-slide
+ **********************************************************/
+import QuestionPage from '../components/question/QuestionPage';
+
+const Question = () => {
+  return <QuestionPage />;
+};
+
+export default Question;
