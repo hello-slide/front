f41f760ff83efc10f6452b2680ff95b9d6c7fa6c
Yuto Watanabe <yuto.w51942@gmail.com>
2021-07-22 18:22:09 +0900

    created changelog page

---
+++ @types/changelog.d.ts
@@ -0,0 +1,13 @@
+/**********************************************************
+ * Change log json type
+ *
+ * @author Yuto Watanabe <yuto.w51942@gmail.com>
+ * @version 1.0.0
+ *
+ * Copyright (C) 2021 hello-slide
+ **********************************************************/
+export default interface ChangeLog {
+  version: string;
+  date: string;
+  text: string;
+}
