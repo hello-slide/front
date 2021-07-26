95d8fe9300b11d9fd26da4b8cd95c0eba68a68d9
Aqua Minato <sweet4182691@gmail.com>
2021-07-26 11:42:17 +0900

    fix: TermsPage.tsx, terms.tsx/ add: terms.md

--- pages/terms.tsx
+++ pages/terms.tsx
@@ -7,9 +7,24 @@
  * Copyright (C) 2021 hello-slide
  **********************************************************/
 import TermsPage from '../components/terms/TermsPage';
+import {readFileSync} from 'fs';
+import path from 'path';
+import {GetStaticProps, InferGetStaticPropsType} from 'next';
 
-const Terms = () => {
-  return <TermsPage />;
+const Terms: React.FC<InferGetStaticPropsType<typeof getStaticProps>> =
+  props => {
+    return <TermsPage contents={props.contents} />;
+  };
+
+export const getStaticProps: GetStaticProps = async () => {
+  const filePath = path.join(process.cwd(), 'contents', 'terms.md');
+  const contents = readFileSync(filePath, 'utf8');
+
+  return {
+    props: {
+      contents: contents,
+    },
+  };
 };
 
 export default Terms;
