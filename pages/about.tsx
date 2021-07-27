54142d5347a2ff331f80aafa107d45a8d2ef9282
Aqua Minato <sweet4182691@gmail.com>
2021-07-27 21:45:02 +0900

    add: about/AboutPage.tsx pages/about.tsx contents/about.md

--- pages/about.tsx
+++ pages/about.tsx
@@ -6,10 +6,25 @@
  *
  * Copyright (C) 2021 hello-slide
  **********************************************************/
+import {readFileSync} from 'fs';
+import path from 'path';
+import {GetStaticProps, InferGetStaticPropsType} from 'next';
 import AboutPage from '../components/about/AboutPage';
 
-const About = () => {
-  return <AboutPage />;
+const About: React.FC<InferGetStaticPropsType<typeof getStaticProps>> =
+  props => {
+    return <AboutPage contents={props.contents} />;
+  };
+
+export const getStaticProps: GetStaticProps = async () => {
+  const filePath = path.join(process.cwd(), 'contents', 'about.md');
+  const contents = readFileSync(filePath, 'utf8');
+
+  return {
+    props: {
+      contents: contents,
+    },
+  };
 };
 
 export default About;
