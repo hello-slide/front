fcfb05a6a88c42b1240ca14c925b93022e8f1f4c
Yuto Watanabe <yuto.w51942@gmail.com>
2021-07-31 16:50:32 +0900

    added setting page

---
+++ components/setting/SettingPage.tsx
@@ -0,0 +1,86 @@
+/**********************************************************
+ * Setting page
+ *
+ * @author Yuto Watanabe <yuto.w51942@gmail.com>
+ * @version 1.0.0
+ *
+ * Copyright (C) 2021 hello-slide
+ **********************************************************/
+import {
+  Box,
+  Avatar,
+  Center,
+  Text,
+  useToast,
+  Modal,
+  ModalOverlay,
+  ModalContent,
+  ModalHeader,
+  ModalFooter,
+  ModalBody,
+  ModalCloseButton,
+  Button,
+  useDisclosure,
+} from '@chakra-ui/react';
+import {useRouter} from 'next/router';
+import {useRecoilState} from 'recoil';
+import {UserDataState} from '../../utils/state/atom';
+
+const SettingPage = () => {
+  const [userData] = useRecoilState(UserDataState);
+  const toast = useToast();
+  const {isOpen, onOpen, onClose} = useDisclosure();
+  const router = useRouter();
+
+  const deleteAccount = () => {
+    router.push('/');
+    toast({
+      title: 'アカウントを削除しました。',
+      status: 'info',
+      duration: 9000,
+      isClosable: true,
+    });
+  };
+
+  return (
+    <Box marginY="3rem">
+      <Center>
+        <Avatar size="xl" src={userData.image} name={userData.name} />
+      </Center>
+      <Center>
+        <Text fontWeight="bold" fontSize="1.5rem" textAlign="center">
+          {userData.name}
+        </Text>
+      </Center>
+      <Center marginTop="5rem">
+        <Text
+          _hover={{textDecoration: 'underline'}}
+          cursor="pointer"
+          onClick={onOpen}
+        >
+          アカウントを削除する
+        </Text>
+      </Center>
+
+      <Modal isOpen={isOpen} onClose={onClose} isCentered>
+        <ModalOverlay />
+        <ModalContent>
+          <ModalHeader>本当に削除しますか？</ModalHeader>
+          <ModalCloseButton size="lg" />
+          <ModalBody>この操作は戻すことはできません。</ModalBody>
+
+          <ModalFooter marginBottom=".3rem">
+            <Button variant="blue" mr={3} onClick={onClose}>
+              キャンセル
+            </Button>
+            <Button colorScheme="blue" onClick={deleteAccount}>
+              削除する
+            </Button>
+          </ModalFooter>
+        </ModalContent>
+      </Modal>
+    </Box>
+  );
+};
+
+export default SettingPage;
