b73eccd4d8bcebd340dcbf8b2d9a4b107ef86d9c
Yuto Watanabe <yuto.w51942@gmail.com>
2021-08-05 01:55:02 +0900

    added delete logic

--- components/dashboard/Slides.tsx
+++ components/dashboard/Slides.tsx
@@ -15,19 +15,24 @@ import {
   ChakraComponent,
   Menu,
   MenuList,
-  MenuItem as ChakuraMenuItem,
+  MenuItem,
   MenuDivider,
+  useDisclosure,
 } from '@chakra-ui/react';
 import React from 'react';
-import {ContextMenu, MenuItem, ContextMenuTrigger} from 'react-contextmenu';
+import {ContextMenu, ContextMenuTrigger} from 'react-contextmenu';
 import {IoAdd, IoTimeOutline} from 'react-icons/io5';
 import Slide from '../../@types/slides';
 import updateDate from '../../utils/updateDate';
+import DeleteSlide from './DeleteSlide';
 
 const Slides: React.FC<{slides: Slide[]; onOpen: () => void}> = ({
   slides,
   onOpen,
 }) => {
+  const deleteSlideModal = useDisclosure();
+  const [deleteSlide, setDeleteSlide] = React.useState<Slide>();
+
   const ListContents: ChakraComponent<'div', {}> = props => {
     return (
       <WrapItem>
@@ -66,69 +71,82 @@ const Slides: React.FC<{slides: Slide[]; onOpen: () => void}> = ({
   };
 
   return (
-    <Wrap
-      spacing="40px"
-      marginX={{base: '5%', sm: '10%', md: '13%', lg: '17%'}}
-    >
-      {slides.map(value => {
-        const createDate = new Date(value.createDate);
-        const lastChangeDate = new Date(value.lastChange);
-        return (
-          <React.Fragment key={value.id}>
-            <ContextMenuTrigger id={value.id}>
-              <ListContents
-                padding=".5rem"
-                onClick={() => {
-                  handleChange(value.id);
-                }}
-                cursor="pointer"
-              >
-                <Text fontWeight="bold" fontSize="1.2rem" marginBottom="1rem">
-                  {value.title}
-                </Text>
-                <Flex alignItems="center" fontSize=".9rem">
-                  <IoTimeOutline size="17" />
-                  <DateInfo
-                    createDate={createDate}
-                    lastChangeDate={lastChangeDate}
-                  />
-                </Flex>
-              </ListContents>
-            </ContextMenuTrigger>
-            <ContextMenu id={value.id}>
-              <Menu isOpen={true}>
-                <MenuList padding={0}>
-                  <Text
-                    fontSize="1rem"
-                    fontWeight="bold"
-                    padding="1rem 0 1rem 1rem"
-                  >
+    <>
+      <Wrap
+        spacing="40px"
+        marginX={{base: '5%', sm: '10%', md: '13%', lg: '17%'}}
+      >
+        {slides.map(value => {
+          const createDate = new Date(value.createDate);
+          const lastChangeDate = new Date(value.lastChange);
+          return (
+            <React.Fragment key={value.id}>
+              <ContextMenuTrigger id={value.id}>
+                <ListContents
+                  padding=".5rem"
+                  onClick={() => {
+                    handleChange(value.id);
+                  }}
+                  cursor="pointer"
+                >
+                  <Text fontWeight="bold" fontSize="1.2rem" marginBottom="1rem">
                     {value.title}
                   </Text>
-                  <MenuDivider margin="0" />
-                  <ChakuraMenuItem padding=".5rem 0 .5rem 1rem">
-                    詳細
-                  </ChakuraMenuItem>
-                  <ChakuraMenuItem height="100%" padding=".5rem 0 .5rem 1rem">
-                    削除
-                  </ChakuraMenuItem>
-                </MenuList>
-              </Menu>
-            </ContextMenu>
-          </React.Fragment>
-        );
-      })}
-      <ListContents onClick={onOpen} cursor="pointer">
-        <Flex
-          alignItems="center"
-          flexDirection="column"
-          justifyContent="center"
-          height="100%"
-        >
-          <IoAdd size="50" />
-        </Flex>
-      </ListContents>
-    </Wrap>
+                  <Flex alignItems="center" fontSize=".9rem">
+                    <IoTimeOutline size="17" />
+                    <DateInfo
+                      createDate={createDate}
+                      lastChangeDate={lastChangeDate}
+                    />
+                  </Flex>
+                </ListContents>
+              </ContextMenuTrigger>
+              <ContextMenu id={value.id}>
+                <Menu isOpen={true}>
+                  <MenuList padding={0}>
+                    <Text
+                      fontSize="1rem"
+                      fontWeight="bold"
+                      padding="1rem 0 1rem 1rem"
+                    >
+                      {value.title}
+                    </Text>
+                    <MenuDivider margin="0" />
+                    {/* TODO */}
+                    {/* <MenuItem padding=".5rem 0 .5rem 1rem">詳細</MenuItem> */}
+                    <MenuItem
+                      height="100%"
+                      padding=".5rem 0 .5rem 1rem"
+                      onClick={() => {
+                        deleteSlideModal.onOpen();
+                        setDeleteSlide(value);
+                      }}
+                    >
+                      削除
+                    </MenuItem>
+                  </MenuList>
+                </Menu>
+              </ContextMenu>
+            </React.Fragment>
+          );
+        })}
+        <ListContents onClick={onOpen} cursor="pointer">
+          <Flex
+            alignItems="center"
+            flexDirection="column"
+            justifyContent="center"
+            height="100%"
+          >
+            <IoAdd size="50" />
+          </Flex>
+        </ListContents>
+      </Wrap>
+      <DeleteSlide
+        slide={deleteSlide}
+        onClose={deleteSlideModal.onClose}
+        isOpen={deleteSlideModal.isOpen}
+      />
+    </>
   );
 };
 
