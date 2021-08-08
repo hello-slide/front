59f5371699216ee830b6da07baaf92a8c6fa9ceb
Yuto Watanabe <yuto.w51942@gmail.com>
2021-08-08 18:28:50 +0900

    added edit page

---
+++ components/edit/EditPage.tsx
@@ -0,0 +1,15 @@
+/**********************************************************
+ * Edit page
+ *
+ * @author Yuto Watanabe <yuto.w51942@gmail.com>
+ * @version 1.0.0
+ *
+ * Copyright (C) 2021 hello-slide
+ **********************************************************/
+import React from 'react';
+
+const EditPage: React.FC<{id: string | string[]}> = ({id}) => {
+  return <>{id}</>;
+};
+
+export default EditPage;
---
+++ pages/edit/[id].tsx
@@ -0,0 +1,24 @@
+/**********************************************************
+ * Edit page.
+ *
+ * @author Yuto Watanabe <yuto.w51942@gmail.com>
+ * @version 1.0.0
+ *
+ * Copyright (C) 2021 hello-slide
+ **********************************************************/
+import {useRouter} from 'next/router';
+import Title from '../../components/common/Title';
+import EditPage from '../../components/edit/EditPage';
+
+const Edit = () => {
+  const router = useRouter();
+  const {id} = router.query;
+  return (
+    <>
+      <Title title="編集" />
+      <EditPage id={id} />
+    </>
+  );
+};
+
+export default Edit;
