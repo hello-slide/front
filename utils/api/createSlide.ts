48b51d5ac8cd9f4abd6e9d657e3f27ea4af9fb6a
Yuto Watanabe <yuto.w51942@gmail.com>
2021-08-13 19:31:12 +0900

    added create slide api logic

--- utils/api/createSlide.ts
+++ utils/api/createSlide.ts
@@ -6,56 +6,28 @@
  *
  * Copyright (C) 2021 hello-slide
  **********************************************************/
-import axios, {AxiosRequestConfig, AxiosError} from 'axios';
-import {updateToken} from './refresh';
+import {AxiosRequestConfig} from 'axios';
+import AbstractApiConnector, {CreateSlideAPI} from './abstractApiConnector';
 
-/**
- * Create Slide API
- *
- * @param {string} token - Session token
- * @param {string} title - Slide title.
- * @param {string} refreshToken - refresh token.
- * @param {(sessionToken: string, refreshToken: string, isFailed?: boolean) => void} updateFunc - Update function.
- * @returns {string} - Slide id.
- */
-export default async function createSlide(
-  token: string,
-  title: string,
-  refreshToken: string,
-  updateFunc: (
-    sessionToken: string,
-    refreshToken: string,
-    isFailed?: boolean
-  ) => void
-): Promise<string> {
-  const config: AxiosRequestConfig = {
-    url: '/slide/create',
-    method: 'post',
-    baseURL: 'https://api.hello-slide.jp/',
-    headers: {
-      'content-type': 'application/json',
-    },
-    data: JSON.stringify({
-      SessionToken: token,
-      Title: title,
-    }),
-    responseType: 'json',
-  };
-
-  try {
-    const response = await axios(config);
+export default class CreateSlide
+  extends AbstractApiConnector
+  implements CreateSlideAPI
+{
+  async run(title: string): Promise<string> {
+    const config: AxiosRequestConfig = {
+      url: '/slide/create',
+      method: 'post',
+      baseURL: this.url,
+      headers: {
+        'content-type': 'application/json',
+      },
+      data: JSON.stringify({
+        SessionToken: this.sessionToken,
+        Title: title,
+      }),
+      responseType: 'json',
+    };
+    const response = await this.connect(config);
     return response.data['slide_id'];
-  } catch (error) {
-    if (
-      (error as AxiosError).code === '401' ||
-      (error as AxiosError).response.status === 401
-    ) {
-      const newToken = await updateToken(updateFunc, refreshToken);
-      return await createSlide(newToken, title, refreshToken, updateFunc);
-    }
-    throw new Error(
-      (error as AxiosError).response.data.status ||
-        (error as AxiosError).message
-    );
   }
 }
