5808d0508b1da46a908cdb8a70a61a88ef372387
Yuto Watanabe <yuto.w51942@gmail.com>
2021-08-16 14:01:33 +0900

    changed empty page desgin

--- components/edit/PageList.tsx
+++ components/edit/PageList.tsx
@@ -15,6 +15,7 @@ import {
   Text,
   useToast,
   Flex,
+  Button,
 } from '@chakra-ui/react';
 import React from 'react';
 import {ContextMenuTrigger, ContextMenu} from 'react-contextmenu';
@@ -171,53 +172,67 @@ const PageList = React.memo<{
 
   return (
     <>
-      <Box
-        width="18rem"
-        height="100%"
-        borderRightWidth="1px"
-        borderRightColor="gray.300"
-      >
-        <ContextMenuTrigger id="pageList">
-          <Box height="calc(100vh - 116px)">
-            <Box
-              width="100%"
-              height="100%"
-              overflowY="scroll"
-              css={{
-                scrollbarWidth: 'none',
-                msOverflowStyle: 'none',
-                '&::-webkit-scrollbar': {
-                  display: 'none',
-                },
-              }}
-            >
-              {pages.map((value, index) => {
-                return (
-                  <PageListItem
-                    page={value}
-                    index={index}
-                    selected={value.id === nowPageId}
-                    key={value.id}
-                  />
-                );
-              })}
-            </Box>
-          </Box>
-        </ContextMenuTrigger>
-        <ContextMenu id="pageList">
-          <Menu isOpen={true}>
-            <MenuList padding={0}>
-              <MenuItem
-                padding=".5rem 0 .5rem 1rem"
-                icon={<IoAdd size="18px" />}
-                onClick={createPageModel.onOpen}
+      {pages.length === 0 ? (
+        <Flex
+          width="18rem"
+          height="calc(100vh - 116px)"
+          borderRightWidth="1px"
+          borderRightColor="gray.300"
+          flexDirection="column"
+          justifyContent="center"
+          alignItems="center"
+        >
+          <Button onClick={createPageModel.onOpen}>新規作成</Button>
+        </Flex>
+      ) : (
+        <Box
+          width="18rem"
+          height="100%"
+          borderRightWidth="1px"
+          borderRightColor="gray.300"
+        >
+          <ContextMenuTrigger id="pageList">
+            <Box height="calc(100vh - 116px)">
+              <Box
+                width="100%"
+                height="100%"
+                overflowY="scroll"
+                css={{
+                  scrollbarWidth: 'none',
+                  msOverflowStyle: 'none',
+                  '&::-webkit-scrollbar': {
+                    display: 'none',
+                  },
+                }}
               >
-                新規作成
-              </MenuItem>
-            </MenuList>
-          </Menu>
-        </ContextMenu>
-      </Box>
+                {pages.map((value, index) => {
+                  return (
+                    <PageListItem
+                      page={value}
+                      index={index}
+                      selected={value.id === nowPageId}
+                      key={value.id}
+                    />
+                  );
+                })}
+              </Box>
+            </Box>
+          </ContextMenuTrigger>
+          <ContextMenu id="pageList">
+            <Menu isOpen={true}>
+              <MenuList padding={0}>
+                <MenuItem
+                  padding=".5rem 0 .5rem 1rem"
+                  icon={<IoAdd size="18px" />}
+                  onClick={createPageModel.onOpen}
+                >
+                  新規作成
+                </MenuItem>
+              </MenuList>
+            </Menu>
+          </ContextMenu>
+        </Box>
+      )}
       <NewPage
         isOpen={createPageModel.isOpen}
         onClose={createPageModel.onClose}
