67e0f1fe130b9acb9ef99f69d668282f3f76fded
Yuto Watanabe <yuto.w51942@gmail.com>
2021-08-22 15:50:02 +0900

    added qr slide contetns

---
+++ components/show/contents/QrCode.tsx
@@ -0,0 +1,34 @@
+/**********************************************************
+ * Qr code slide contents
+ *
+ * @author Yuto Watanabe <yuto.w51942@gmail.com>
+ * @version 1.0.0
+ *
+ * Copyright (C) 2021 hello-slide
+ **********************************************************/
+import {Flex, Text, Heading} from '@chakra-ui/react';
+import QRCode from 'qrcode.react';
+import React from 'react';
+import {sessionApiLink} from '../../../utils/api/links';
+
+const QrCode: React.FC<{session: string}> = ({session}) => {
+  const link = `${sessionApiLink}${session}`;
+  return (
+    <Flex
+      flexDirection="column"
+      justifyContent="center"
+      alignItems="center"
+      height="100%"
+    >
+      <Heading fontSize="3rem">一緒に参加しよう！</Heading>
+      <Flex justifyContent="center" alignItems="center" marginTop="7rem">
+        <QRCode value={link} size={300} />
+        <Text fontSize="2.5rem" fontWeight="bold" marginLeft="4rem">
+          {link}
+        </Text>
+      </Flex>
+    </Flex>
+  );
+};
+
+export default QrCode;
