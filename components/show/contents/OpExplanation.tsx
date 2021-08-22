4d22d3c8cd29cc0d7bf233f6d86c2e8e722cd71d
Yuto Watanabe <yuto.w51942@gmail.com>
2021-08-22 16:34:57 +0900

    added operation explanation slide contents

---
+++ components/show/contents/OpExplanation.tsx
@@ -0,0 +1,64 @@
+/**********************************************************
+ * Operation explanation slide contents.
+ *
+ * @author Yuto Watanabe <yuto.w51942@gmail.com>
+ * @version 1.0.0
+ *
+ * Copyright (C) 2021 hello-slide
+ **********************************************************/
+import {Flex, Heading, Kbd, Stack, Spacer, Text} from '@chakra-ui/react';
+import React from 'react';
+
+const OpExplanation: React.FC<{slideTitle: string}> = ({slideTitle}) => {
+  const KeyMap: React.FC<{text: string; keys: string[]}> = ({text, keys}) => {
+    return (
+      <Flex alignItems="center">
+        <Text fontSize="1.5rem">{text}</Text>
+        <Spacer />
+        <Text fontSize="1.5rem" fontWeight="bold">
+          {keys.map((value, index) => {
+            return (
+              <>
+                <Kbd key={value} marginX=".2rem">
+                  {value}
+                </Kbd>
+                {index !== keys.length - 1 && '+'}
+              </>
+            );
+          })}
+        </Text>
+      </Flex>
+    );
+  };
+
+  const ExplanationContent: React.FC<{title: string}> = props => {
+    return (
+      <Stack direction="column" spacing="5px" width="20rem">
+        <Heading marginBottom="2rem">{props.title}</Heading>
+        {props.children}
+      </Stack>
+    );
+  };
+
+  return (
+    <Flex
+      flexDirection="column"
+      justifyContent="center"
+      alignItems="center"
+      height="100%"
+    >
+      <Stack direction="row" spacing="5rem">
+        <ExplanationContent title="操作説明">
+          <KeyMap text="次のページに移動する" keys={['→']} />
+          <KeyMap text="前のページに戻る" keys={['←']} />
+          <KeyMap text="終了する" keys={['Esc']} />
+        </ExplanationContent>
+        <ExplanationContent title="基本情報">
+          <Text fontSize="1.5rem">スライド名: {slideTitle}</Text>
+        </ExplanationContent>
+      </Stack>
+    </Flex>
+  );
+};
+
+export default OpExplanation;
