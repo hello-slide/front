e2804cd2d193a786deca55b84a8b7c74f3baca4e
Yuto Watanabe <yuto.w51942@gmail.com>
2021-08-30 19:45:45 +0900

    Change the processing at full screen with safari

--- components/show/ShowDisplay.tsx
+++ components/show/ShowDisplay.tsx
@@ -6,7 +6,7 @@
  *
  * Copyright (C) 2021 hello-slide
  **********************************************************/
-import {Box} from '@chakra-ui/react';
+import {Box, useToast} from '@chakra-ui/react';
 import React from 'react';
 import NoSSR from 'react-no-ssr';
 import {useSetRecoilState} from 'recoil';
@@ -17,26 +17,49 @@ import ShowController from './ShowController';
 const ShowDisplay: React.FC<{id: string}> = ({id}) => {
   const fullScreenRef = React.useRef();
   const [isFull, setIsFull] = React.useState(true);
+  const [safari, setSafari] = React.useState(false);
   const setShow = useSetRecoilState(ShowState);
+  const toast = useToast();
+
+  const escEvent = (event: KeyboardEvent) => {
+    if (event.code === 'Escape') {
+      setIsFull(false);
+    }
+  };
 
   React.useEffect(() => {
     let isMounted = true;
     if (screenfull.isEnabled) {
       screenfull.request(fullScreenRef.current);
-    }
 
-    if (screenfull.isEnabled) {
       screenfull.on('change', () => {
         if (isMounted) {
           setIsFull((screenfull as {isFullscreen: boolean}).isFullscreen);
         }
       });
+
+      screenfull.on('error', () => {
+        setSafari(true);
+      });
     }
 
     return () => {
+      document.removeEventListener('keydown', escEvent, false);
       isMounted = false;
     };
   }, []);
+
+  React.useEffect(() => {
+    if (safari) {
+      toast({
+        title: 'お使いのブラウザは手動でフルスクリーンにする必要があります',
+        description: 'Control+Command+Fを押してフルスクリーンにしてください。',
+        status: 'info',
+        isClosable: true,
+      });
+      document.addEventListener('keydown', escEvent, false);
+    }
+  }, [safari]);
 
   React.useEffect(() => {
     if (!isFull) {
