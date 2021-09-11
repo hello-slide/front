4642a600b72670ffb2c3c86ed77a80bbd1a352cd
Yuto Watanabe <yuto.w51942@gmail.com>
2021-09-12 00:48:55 +0900

    change socket utils

--- utils/socket/visitorSocket.ts
+++ utils/socket/visitorSocket.ts
@@ -1,16 +1,17 @@
 /**********************************************************
- * connect to host by websocket.
+ * connect to visitor by websocket.
  *
  * @author Yuto Watanabe <yuto.w51942@gmail.com>
  * @version 1.0.0
  *
  * Copyright (C) 2021 hello-slide
  **********************************************************/
+
 import AbstractSocketClient from './abstractSocket';
 
-export default class HostSocket extends AbstractSocketClient {
+export default class VisitorSocket extends AbstractSocketClient {
   constructor() {
-    const path = '/sync/host';
+    const path = '/sync/visitor';
 
     super(path);
   }
