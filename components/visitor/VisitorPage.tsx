91b485b261f136cf0f21749f378ecda8010cc8c2
Yuto Watanabe <yuto.w51942@gmail.com>
2021-09-12 00:46:19 +0900

    added visitor page

---
+++ components/visitor/VisitorPage.tsx
@@ -0,0 +1,15 @@
+/**********************************************************
+ * Visitor page.
+ *
+ * @author Yuto Watanabe <yuto.w51942@gmail.com>
+ * @version 1.0.0
+ *
+ * Copyright (C) 2021 hello-slide
+ **********************************************************/
+import React from 'react';
+
+const VisitorPage: React.FC<{id: string | string[]}> = ({id}) => {
+  return <>{id}</>;
+};
+
+export default VisitorPage;
